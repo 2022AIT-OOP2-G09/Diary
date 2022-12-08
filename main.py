1bfc840b446e034ea48dd700b48d618549ec9754
k19041 <k19041kk@aitech.ac.jp>
2022-12-08 16:09:16 +0900

    くまもとの日記

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from diaries.DiarySample import DiarySample
+from diaries.k19041Diary import k19041Diary
 
 # ↓のリストには、メンバーの各⽇記が格納されます。
 diaries = [
     DiarySample(),
+    k19041Diary(),
+    
 ]
 
 for d in diaries:
