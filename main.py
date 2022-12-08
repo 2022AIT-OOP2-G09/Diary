38b428159b33e7a0eb4812ac61c312966b7dcea3
iu-A <imaizumi2002@gmail.com>
2022-12-08 15:52:52 +0900

    K21016日記を追加

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.K21016Diary import K21016Diary
 
 # ↓のリストには、メンバーの各⽇記が格納されます。
 diaries = [
     DiarySample(),
+    K21016Diary(),
 ]
 
 for d in diaries:
